2d1cc306b29154f5f3e4de0d5e1d02c2e5106b27
Tim Voßhenrich <94936637+T1mVo@users.noreply.github.com>
2025-12-11 15:17:45 +0100

    Adjust package script
    Typst Universe does not support GFM task lists.

--- scripts/package.sh
+++ scripts/package.sh
@@ -9,3 +9,11 @@ VERSION=$(awk -F'"' '/^version *=/ {print $2}' $DIR/../src-typst/typst.toml)
 mkdir -p "$DIR/typed/$VERSION"
 
 cp -Lr $DIR/../src-typst/** $DIR/typed/$VERSION
+
+if sed --version >/dev/null 2>&1; then
+    # GNU sed (Linux)
+    sed -i 's/\[ \]/❌/g; s/\[[xX]\]/✅/g' "$DIR/typed/$VERSION/README.md"
+else
+    # BSD sed (macOS)
+    sed -i '' 's/\[ \]/❌/g; s/\[[xX]\]/✅/g' "$DIR/typed/$VERSION/README.md"
+fi
